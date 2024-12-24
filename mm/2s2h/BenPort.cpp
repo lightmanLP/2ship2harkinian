b0dad843a573521be3fc399b95fb512e9582b230
lightmanLP <liteman1000@gmail.com>
2024-12-25 05:54:59 +0700

    fix invalid menu name

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -1637,7 +1637,7 @@ extern "C" void OTRControllerCallback(uint8_t rumble) {
     static std::shared_ptr<BenInputEditorWindow> controllerConfigWindow = nullptr;
     if (controllerConfigWindow == nullptr) {
         controllerConfigWindow = std::dynamic_pointer_cast<BenInputEditorWindow>(
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Input Editor"));
+            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("2S2H Input Editor"));
         // TODO: Add SoH Controller Config window rumble testing to upstream LUS config window
         //       note: the current implementation may not be desired in LUS, as "true" rumble support
         //             using osMotor calls is planned: https://github.com/Kenix3/libultraship/issues/9
